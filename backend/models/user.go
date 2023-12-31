3e310042266e46adb97f7a3427611e2b63bb6aca
bigge <pongpich.sing@gmail.com>
2023-12-31 17:50:43 +0700

    remove unused code

--- backend/models/user.go
+++ backend/models/user.go
@@ -5,8 +5,6 @@ import (
 	"gorm.io/gorm"
 )
 
-var InMemoryUsers = make(map[string]User)
-
 type User struct {
 	gorm.Model
 	Username string `gorm:"unique;not null"`
@@ -16,20 +14,4 @@ type User struct {
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(hashedPassword), err
-}
-
-func CheckPasswordHash(username, password string) bool {
-	hashedPassword := InMemoryUsers[username].Password
-	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)); err != nil {
-		return false
-	}
-	return true
-}
-
-func CheckPassword(username, password string) bool {
-	_, exists := InMemoryUsers[username]
-	if !exists {
-		return false
-	}
-	return CheckPasswordHash(username, password)
 }
