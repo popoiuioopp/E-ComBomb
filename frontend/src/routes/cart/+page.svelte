1ce4ce04d3d3b05ce2c4de61614b5d89c60bab5e
bigge <pongpich.sing@gmail.com>
2024-03-05 16:04:58 +0700

    initial cart page

--- frontend/src/routes/cart/+page.svelte
+++ frontend/src/routes/cart/+page.svelte
@@ -1,1 +1,14 @@
-<div>Hello Cart</div>
+<h1 class="cart-header">Shopping Cart</h1>
+
+<hr class="divider" />
+
+<style>
+	.cart-header {
+		display: flex;
+		justify-content: center;
+	}
+
+	.divider {
+		width: 95%;
+	}
+</style>
